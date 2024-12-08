d5b9ba02bb1ea07daa3d101eac7cb5985230d815
Russell Johnson <russ@johnsonville.net>
2024-12-08 15:16:46 -0600

    Day 7 and 8 checkin

--- Day_7/Query.sql
+++ Day_7/Query.sql
@@ -1,1 +1,25 @@
-
+WITH RankedElves AS (
+    SELECT
+        elf_id,
+        primary_skill,
+        years_experience,
+        ROW_NUMBER() OVER (PARTITION BY primary_skill ORDER BY years_experience DESC, elf_id) AS max_rank,
+        ROW_NUMBER() OVER (PARTITION BY primary_skill ORDER BY years_experience ASC, elf_id) AS min_rank
+    FROM workshop_elves
+),
+MaxMinElves AS (
+    SELECT
+        MAX(elf_id) FILTER (WHERE max_rank = 1) AS elf_max_id,
+        MAX(elf_id) FILTER (WHERE min_rank = 1) AS elf_min_id,
+        primary_skill
+    FROM RankedElves
+    WHERE max_rank = 1 OR min_rank = 1
+    GROUP BY primary_skill
+)
+SELECT
+    elf_max_id AS elf_1_id,
+    elf_min_id AS elf_2_id,
+    primary_skill
+FROM MaxMinElves
+WHERE elf_max_id != elf_min_id -- Ensure they are not the same elf
+ORDER BY primary_skill, elf_1_id, elf_2_id;
---
+++ Day_8/Query.sql
@@ -0,0 +1,23 @@
+-- Borrowed method from here: https://learnsql.com/blog/sql-recursive-cte/
+
+WITH RECURSIVE staff_hierarchy AS (
+  SELECT staff_id, staff_name, manager_id, 0 AS hierarchy_level
+  FROM staff
+  WHERE manager_id IS NULL
+ 
+  UNION ALL
+   
+  SELECT s.staff_id, s.staff_name, s.manager_id, hierarchy_level + 1
+  FROM staff s, staff_hierarchy sh
+  WHERE s.manager_id = sh.staff_id
+)
+ 
+SELECT   sh.staff_id AS employee_first_name,
+       sh.staff_name AS employee_last_name,
+       s.staff_id AS boss_first_name,
+       s.staff_name AS boss_last_name,
+       hierarchy_level+ 1
+FROM staff_hierarchy sh
+LEFT JOIN staff s
+ON sh.manager_id = s.staff_id
+ORDER BY sh.hierarchy_level DESC, sh.manager_id;
